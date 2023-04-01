d045d9a2c0a6487277aa78d6e03ff78c7df97319
vrutik <vrutiksurti8@gmail.com>
2023-04-01 09:55:19 +0530

    files added

---
+++ App.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement("h1",
+// {id:"heading",
+// abc:"xyz"},
+// "hello world from React!")
+
+const parent = React.createElement("div",{id:"parent"},
+                 React.createElement("div",{id:"child"},
+                [
+                React.createElement("h1",{},"This is h1 tag"),
+                React.createElement("h2",{},"This is h2 tag")
+                ]
+                )
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
