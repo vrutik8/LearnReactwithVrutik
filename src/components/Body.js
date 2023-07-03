9066af990dabee87a0fc7d60d177c2390a4fd388
vrutik <surtivrutik18@gmail.com>
2023-07-03 23:07:28 +0530

    committed six

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,73 +1,25 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard'
 import resList from '../utils/mockData'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import Shimmer from './Shimmer'
 
 const Body = () => {
-      const[listOfRestauranats,setlistOfRestauranats] = useState(resList);
+      const[listOfRestauranats,setlistOfRestauranats] = useState([]);
+      useEffect(()=>{
+        fetchData();
+      },[]);
 
-    // const[listOfRestaurants,setListOfRestaurants] = useState({
+      // https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING
 
+      const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        setlistOfRestauranats(json?.data?.cards[2]?.data?.data?.cards);
+      }
 
-//     onClick={() => {
-//       const filtered = listOfRestaurants.filter((res) => res?.data?.avgRating > 4);
-//       setListOfRestaurants(filtered);
-//       console.log(filtered);
-// }}
-//   let listOfRestauranats2 = [{
-  
-
-//       "data": {
-  
-//         "id": "2",
-//         "name": "Hotels villa",
-       
-//         "cloudinaryImageId": "qltgnkyywuo5gmnpqzbm",
-//         "cuisines": [
-//           "North Indian",
-//           "Kebabs",
-//           "Biryani"
-//         ],
-//         "costForTwo": 48000,
-//         "deliveryTime": 32,
-//         "avgRating": "3.0",}
-//       },
-// {
-//       "data": {
-  
-//         "id": "3",
-//         "name": "Hotels MACD",
-       
-//         "cloudinaryImageId": "qltgnkyywuo5gmnpqzbm",
-//         "cuisines": [
-//           "North Indian",
-//           "Kebabs",
-//           "Biryani"
-//         ],
-//         "costForTwo": 48000,
-//         "deliveryTime": 32,
-//         "avgRating": "4.5",}
-//       },
-
-// {
-//       "data": {
- 
-//         "id": "1",
-//         "name": "voter Empire",
-       
-//         "cloudinaryImageId": "qltgnkyywuo5gmnpqzbm",
-//         "cuisines": [
-//           "North Indian",
-//           "Kebabs",
-//           "Biryani"
-//         ],
-//         "costForTwo": 45000,
-//         "deliveryTime": 32,
-//         "avgRating": "4.0"
-//       },
-//     }
-//     ]
-    return(
+      return (listOfRestauranats.length===0) ? <Shimmer/> : (
         <div className="body-container">
             <div className="filter">
             <button className="filter-btn" onClick={()=>{
---
+++ src/components/Shimmer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='shimmer-container'>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    <div className='shimmer-card'>Shimmer</div>
+    </div>
+  )
+}
+
+export default Shimmer
