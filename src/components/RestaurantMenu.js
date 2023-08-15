272f1f4a14f91517b2eee28a534ef004e0b3ff2a
vrutik <surtivrutik18@gmail.com>
2023-08-16 00:47:57 +0530

    restaurant detailed page mapping

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -14,18 +14,32 @@ const RestaurantMenu = () => {
         const json = await data.json();
         console.log(json);
         setResInfo(json.data)
-
     }
 
-    // const { name, costForTwoMessage, cuisines} = resInfo?.cards[0]?.card?.card?.info;
 
-  return resInfo === null ? (<Shimmer/>) : (
+    // const { name } =;
+
+
+    // data.cards[3].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards
+
+ if(resInfo===null) return <Shimmer/>;
+  return  (
     <div className='menu'>
-        <h1> {resInfo?.cards[0]?.card?.card?.info?.name}</h1>
-        {/* <h1> {costForTwoMessage}</h1> */}
-        <h1> {resInfo?.cards[0]?.card?.card?.info?.cuisines.join(",")} - {resInfo?.cards[0]?.card?.card?.info?.costForTwoMessage}</h1>
+        <h1> { resInfo?.cards[0]?.card?.card?.info?.name}</h1>
+        <p> { resInfo?.cards[0]?.card?.card?.info?.cuisines.join(",") } - { resInfo?.cards[0]?.card?.card?.info?.costForTwoMessage}</p>
+        <h1>Menu</h1>
+        <ul className='menu'>
+            {/* <li> {resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards }</li> */}
+            <li>{resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[0]?.card?.info?.name}</li><br />
+            <li>{resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[1]?.card?.info?.name}</li><br />
+            <li>{resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[3]?.card?.info?.name}</li><br />
+            <li>{resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[4]?.card?.info?.name}</li><br />
+            <li>{resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[4]?.card?.info?.name}</li><br />
+            <li>{resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[5]?.card?.info?.name}</li><br />
+    
+        </ul>
         </div>
   )
 }
-
+// data.cards[0].card.card.info.costForTwoMessage
 export default RestaurantMenu
